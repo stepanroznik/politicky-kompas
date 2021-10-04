6ef86074dd51bdff091bca5e7d151e64ebb4e528
Stepan Roznik <roznik@navisys.cz>
2021-10-04 23:37:19 +0200

    feat: procentuální šance na postup do sněmovny

--- src/components/Comparison.vue
+++ src/components/Comparison.vue
@@ -4,7 +4,7 @@
             v-for="result, index in results"
             :key="result.party.id"
             class="py-2.5 px-4 border-4 bg-gray-200 rounded overflow-hidden relative flex flex-row items-center"
-        >   
+        >
             <span 
                 class="flex flex-shrink-0 w-12 h-12 bg-contain border-2 border-white bg-white rounded-md mr-2.5"
                 :style="{backgroundImage: `url(${require(`@/assets/parties/${result.party.id}.png`)})`}"
@@ -14,8 +14,34 @@
                     <span class="hidden sm:block md:hidden lg:block">{{ result.party.name }}</span>
                     <span class="block sm:hidden md:block lg:hidden">{{ result.party.abbreviation }}</span>
                 </dt>
-                <dd class="mt-0.5 text-2xl font-semibold text-gray-700">
-                    {{ result.percentage }} %
+                <dd class="mt-0.5 text-2xl font-semibold text-gray-700 grid grid-percentage z-10">
+                    <span>
+                        {{ result.percentage }} %
+                    </span>
+                    <span class="flex text-sm text-gray-400 group">
+                        <span class="shadow-md text-sm font-medium text-gray-600 z-10 select-none absolute left-1/2 top-1/2 -translate-y-1/2 bg-white rounded py-1 px-2 mr-0.5 transition-all transform -translate-x-1/2 scale-x-0 opacity-0 group-hover:scale-100 group-hover:opacity-100">
+                            {{ 
+                                (bigParties.includes(result.party.abbreviation)) ? 'Zaručeně projde do sněmovny' :
+                                (smallParties.includes(result.party.abbreviation)) ? 'Malá šance na postup do sněmovny' :
+                                'Téměř nulová šance na postup do sněmovny'
+                            }}
+                        </span>
+                        <span class="flex items-end p-1 border bg-gray-100 rounded-sm select-none">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                class="h-4 w-4 mr-1"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                            >
+                                <path d="M2 11a1 1 0 011-1h2a1 1 0 011 1v5a1 1 0 01-1 1H3a1 1 0 01-1-1v-5zM8 7a1 1 0 011-1h2a1 1 0 011 1v9a1 1 0 01-1 1H9a1 1 0 01-1-1V7zM14 4a1 1 0 011-1h2a1 1 0 011 1v12a1 1 0 01-1 1h-2a1 1 0 01-1-1V4z" />
+                            </svg>
+                            {{
+                                (bigParties.includes(result.party.abbreviation)) ? '> 99' :
+                                (smallParties.includes(result.party.abbreviation)) ? '&lt; 50' :
+                                '&lt; 1'
+                            }}
+                        </span>
+                    </span>
                 </dd>
             </span>
             <span 
@@ -43,7 +69,9 @@ export default defineComponent({
     data() {
         return {
             results: [] as any,
-            locationMarker
+            locationMarker,
+            bigParties: ['SPOLU', 'Piráti+STAN', 'ANO', 'SPD'],
+            smallParties: ['PŘÍSAHA', 'ČSSD', 'KSČM', 'TSS'],
         };
     },
     computed: {},
@@ -59,3 +87,9 @@ export default defineComponent({
     }
 });
 </script>
+
+<style scoped lang="postcss">
+.grid-percentage {
+    grid-template-columns: 7rem auto;
+}
+</style>
